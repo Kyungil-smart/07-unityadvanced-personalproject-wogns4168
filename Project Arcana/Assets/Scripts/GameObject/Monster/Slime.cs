67e871855706c771bf53d03ca65edbf32210a0a0
Jaehoon Jeong <wogns4168@naver.com>
2026-02-26 17:02:37 +0900

    깃이그노어 내용 추가

---
+++ Project Arcana/Assets/Scripts/GameObject/Monster/Slime.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Slime : MonoBehaviour, ITargetable
+{
+    public void OnHoverEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHoverExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSelect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Project Arcana/Assets/Scripts/GameObject/Player/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour, ITargetable
+{
+    public void OnHoverEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHoverExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSelect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
