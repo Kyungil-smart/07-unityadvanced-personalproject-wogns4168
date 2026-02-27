7963d819457d1b94f9a55a63a50f3015299cde49
Jaehoon Jeong <wogns4168@naver.com>
2026-02-27 11:10:42 +0900

    Turn StatePattern 기본 구조 생성

---
+++ Project Arcana/Assets/Scripts/Util/Turn/PlayerState.cs
@@ -0,0 +1,17 @@
+﻿public class PlayerState : ITurn
+{
+    public void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
